f81a233327dd2a8d784198df190ab5b6285f1dbd
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-04 12:39:18 -0700

    imports random number

--- game.py
+++ game.py
@@ -1,5 +1,9 @@
 """A number-guessing game."""
 
 print "Hey, what's your name?"
-user_name = raw_input ("Input your name: ")
-print "Your name is %s!" %(user_name) 
+user_name = raw_input("Input your name: ")
+print "Your name is %s!" % (user_name)
+
+import random
+random_number = random.randint(0, 101)
+print random_number
