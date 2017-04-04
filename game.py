58840eef9db9695bf867446737316868ebe7a688
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-04 12:49:12 -0700

    Got guess and checked

--- game.py
+++ game.py
@@ -7,3 +7,10 @@ print "Your name is %s!" % (user_name)
 import random
 random_number = random.randint(0, 101)
 print random_number
+
+guess_number = raw_input("Please enter your number: ")
+guess_number = int(guess_number)
+while guess_number < 0 or guess_number > 100:
+    print "Your number is not between 0 and 100."
+    guess_number = raw_input("Please enter a new number between 0 and 100: ")
+    guess_number = int(guess_number)
