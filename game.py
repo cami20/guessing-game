3d09fd2645081df4b257af3fd0a6f607199c4a50
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-04 13:02:01 -0700

    checking to see if int

--- game.py
+++ game.py
@@ -9,8 +9,20 @@ random_number = random.randint(0, 101)
 print random_number
 
 guess_number = raw_input("Please enter your number: ")
-guess_number = int(guess_number)
+try:
+    guess_number = int(guess_number)
+except:
+    print "That is not a number."
+    guess_number = raw_input("Please enter a new number between 0 and 100: ")
+    guess_number = int(guess_number)
+
+while type(guess_number) == float:
+    print "We have rounded your number."
+
 while guess_number < 0 or guess_number > 100:
     print "Your number is not between 0 and 100."
     guess_number = raw_input("Please enter a new number between 0 and 100: ")
     guess_number = int(guess_number)
+
+
+"""We are having trouble with decimals. Keeps saying they are not numbers."""
