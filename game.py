1f386bf2901ead26aa05297706b0af8effc00d34
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-04 15:40:15 -0700

    Added checks to see if valid number and rounded.

--- game.py
+++ game.py
@@ -6,9 +6,13 @@ print "Your name is %s!" % (user_name)
 
 import random
 random_number = random.randint(0, 101)
-print random_number
 
 guess_number = raw_input("Please enter your number: ")
+if "." in guess_number:
+    print "this is a float we are rounding your number."
+    guess_number = float(guess_number)
+    guess_number = round(guess_number)
+
 try:
     guess_number = int(guess_number)
 except:
@@ -16,13 +20,8 @@ except:
     guess_number = raw_input("Please enter a new number between 0 and 100: ")
     guess_number = int(guess_number)
 
-while type(guess_number) == float:
-    print "We have rounded your number."
 
 while guess_number < 0 or guess_number > 100:
     print "Your number is not between 0 and 100."
     guess_number = raw_input("Please enter a new number between 0 and 100: ")
     guess_number = int(guess_number)
-
-
-"""We are having trouble with decimals. Keeps saying they are not numbers."""
