b6163b874d8a71ab8cbdd2ede091ab725e137abc
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-04 17:26:18 -0700

    removed some bugs

--- game.py
+++ game.py
@@ -25,25 +25,23 @@ guess_number = (raw_input("Please enter your number: "))
 '''calls function to check if valid guess'''
 guess_number = check_valid_guess(guess_number)
 
-'''checks to see if guess is withing the range'''
-while guess_number < 0 or guess_number > 100:
-    print "Your number is not between 0 and 100."
-    guess_number = raw_input("Please enter a new number between 0 and 100: ")
-    guess_number = check_valid_guess(guess_number)
-
-
 tries = 1
 
 '''checks if guessed correctly and allows to guess again'''
 while guess_number != random_number:
+    '''checks to see if guess is withing the range'''
+    while guess_number < 0 or guess_number > 100:
+        print "Your number is not between 0 and 100."
+        guess_number = raw_input("Please enter a new number between 0 and 100: ")
+        guess_number = check_valid_guess(guess_number)
     tries += 1
     print "That's not it!"
     if guess_number < random_number:
         print "that's too low!"
     else:
         print "that's too high!"
-    guess_number = int(raw_input("Please enter another number: "))
-    check_valid_guess(guess_number)
+    guess_number = raw_input("Please enter another number: ")
+    guess_number = check_valid_guess(guess_number)
 
 print "You got it!"
 print "It took %d tries." % (tries)
